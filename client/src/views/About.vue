215147fe3d6cdd112965557a2d4f804dbe9b880a
dnfwlxo11 <dnfwlxo11@naver.com>
2021-08-20 17:50:09 +0900

    edit: parse code

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -81,51 +81,76 @@
                 console.log(res.data)
             },
 
+            getHearingLoss(data) {
+                const ptaAc = [data["pta_ac_500"] ? Number(data["pta_ac_500"]):null, data["pta_ac_1000"] ? Number(data["pta_ac_1000"]):null, data["pta_ac_2000"] ? Number(data["pta_ac_2000"]):null, data["pta_ac_4000"] ? Number(data["pta_ac_4000"]):null]
+                const ptaBc = [data["pta_bc_500"] ? Number(data["pta_bc_500"]):null, data["pta_bc_1000"] ? Number(data["pta_bc_1000"]):null, data["pta_bc_2000"] ? Number(data["pta_bc_2000"]):null, data["pta_bc_4000"] ? Number(data["pta_bc_4000"]):null]
+                const acLen = ptaAc.filter(item => { return item != null }).length
+                const bcLen = ptaBc.filter(item => { return item != null }).length
+
+                if (acLen!=4 || bcLen%4) return 4
+                else if (!bcLen) return 0
+                else {
+                    const acAvg = (ptaAc[0]+2*ptaAc[1]+2*ptaAc[2]+ptaAc[3])/6
+                    const bcAvg = (ptaBc[0]+2*ptaBc[1]+2*ptaBc[2]+ptaBc[3])/6
+                    if (bcAvg > 65) return 1
+                    else if (ptaAc[0]>25 || ptaAc[1]>25 || ptaAc[2]>25 || ptaAc[3]>25) {
+                        if (acAvg - bcAvg < 15) return 1
+                        else {
+                            if (bcAvg <= 25) return 2
+                            else return 3
+                        }
+                    } else return 0
+                }
+            },
+
             setOriginal() {
                 const exportData = this.excelData.reduce((acc, item) => {
-                    if (item["등록번호"]) {
-                        const baseCol = this.excelCol
-                        const variableCol = [0, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, 0, 0, 0, 0, null, "text", ""]
-                        const rightCol = baseCol.slice(1, 26).concat(variableCol).concat(baseCol.slice(46, 47)).concat(baseCol.slice(48, 50))
-                        const rightCause = baseCol.slice(52, 55).map(arr => { if (item[arr]) return this.cause_loss[item[arr]] }).filter(arr => { return arr != undefined })
-                        const rightEar = baseCol.slice(58, 61).map(arr => { if (item[arr]) return this.text_eardr[item[arr]] }).filter(arr => { return arr != undefined })
-                        const leftCol = baseCol.slice(1, 6).concat(baseCol.slice(26, 46)).concat(variableCol).concat(baseCol.slice(47, 48)).concat(baseCol.slice(50, 52))
-                        const leftCause = baseCol.slice(55, 58).map(arr => { if (item[arr]) return this.cause_loss[item[arr]] }).filter(arr => { return arr != undefined })
-                        const leftEar = baseCol.slice(61, 64).map(arr => { if (item[arr]) return this.text_eardr[item[arr]] }).filter(arr => { return arr != undefined })
+                    const baseCol = this.excelCol
+                    const variableCol = [0, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, 0, 0, 0, 0, null, "text", ""]
+                    const rightCol = baseCol.slice(1, 26).concat(variableCol).concat(baseCol.slice(46, 47)).concat(baseCol.slice(48, 50))
+                    const rightCause = baseCol.slice(52, 55).map(arr => { if (item[arr]) return this.cause_loss[item[arr]] }).filter(arr => { return arr != undefined })
+                    const rightEar = baseCol.slice(58, 61).map(arr => { if (item[arr]) return this.text_eardr[item[arr]] }).filter(arr => { return arr != undefined })
+                    const leftCol = baseCol.slice(1, 6).concat(baseCol.slice(26, 46)).concat(variableCol).concat(baseCol.slice(47, 48)).concat(baseCol.slice(50, 52))
+                    const leftCause = baseCol.slice(55, 58).map(arr => { if (item[arr]) return this.cause_loss[item[arr]] }).filter(arr => { return arr != undefined })
+                    const leftEar = baseCol.slice(61, 64).map(arr => { if (item[arr]) return this.text_eardr[item[arr]] }).filter(arr => { return arr != undefined })
 
-                        let cnt = 0
-                        const rightData = rightCol.reduce((right, value) => {
-                            if (cnt > 24 && cnt < 57) right[this.parseCol[cnt]] = value
-                            else right[this.parseCol[cnt]] = item[value]
-                            cnt += 1
-                            return right
-                        }, {})
+                    console.log(rightCol)
 
-                        cnt = 0
-                        const leftData = leftCol.reduce((left, value) => {
-                            if (cnt > 24 && cnt < 57) left[this.parseCol[cnt]] = value
-                            else left[this.parseCol[cnt]] = item[value]
-                            cnt += 1
-                            return left
-                        }, {})
+                    let cnt = 0
+                    const rightData = rightCol.reduce((right, value) => {
+                        if (cnt > 24 && cnt < 57) right[this.parseCol[cnt]] = value
+                        else right[this.parseCol[cnt]] = item[value] != null ? item[value] : null
+                        cnt += 1
+                        return right
+                    }, {})
 
-                        for (let i=0;i<rightCause.length;i++) rightData[rightCause[i]] = 1
-                        rightData["cause_loss_priority"] += rightCause.join(",")
-                        for (let i=0;i<rightEar.length;i++) rightData[rightEar[i]] = 1
-                        rightData["text_eardr_priority"] += rightEar.join(",")
-                        for (let i=0;i<leftCause.length;i++) leftData[leftCause[i]] = 1
-                        leftData["cause_loss_priority"] += leftCause.join(",")
-                        for (let i=0;i<leftEar.length;i++) leftData[leftEar[i]] = 1
-                        leftData["text_eardr_priority"] += leftEar.join(",")
+                    cnt = 0
+                    const leftData = leftCol.reduce((left, value) => {
+                        if (cnt > 24 && cnt < 57) left[this.parseCol[cnt]] = value
+                        else left[this.parseCol[cnt]] = item[value] != null ? item[value] : null
+                        cnt += 1
+                        return left
+                    }, {})
 
-                        rightData["ear_select"] = 0
-                        leftData["ear_select"] = 1
-                        rightData["sex"] == "남" ? rightData["sex"] = 0 : rightData["sex"] = 1
-                        leftData["sex"] == "남" ? leftData["sex"] = 0 : leftData["sex"] = 1
+                    for (let i=0;i<rightCause.length;i++) rightData[rightCause[i]] = 1
+                    rightData["cause_loss_priority"] += rightCause.join(",")
+                    for (let i=0;i<rightEar.length;i++) rightData[rightEar[i]] = 1
+                    rightData["text_eardr_priority"] += rightEar.join(",")
+                    for (let i=0;i<leftCause.length;i++) leftData[leftCause[i]] = 1
+                    leftData["cause_loss_priority"] += leftCause.join(",")
+                    for (let i=0;i<leftEar.length;i++) leftData[leftEar[i]] = 1
+                    leftData["text_eardr_priority"] += leftEar.join(",")
 
-                        acc.push(rightData)
-                        acc.push(leftData)
-                    }
+                    rightData["ear_select"] = 0
+                    leftData["ear_select"] = 1
+                    rightData["sex"] == "남" || rightData["sex"] == "남자" ? rightData["sex"] = 0 : rightData["sex"] = 1
+                    leftData["sex"] == "남" || leftData["sex"] == "남자" ? leftData["sex"] = 0 : leftData["sex"] = 1
+
+                    rightData["hearing_loss"] = this.getHearingLoss(rightData)
+                    leftData["hearing_loss"] = this.getHearingLoss(leftData)
+
+                    acc.push(rightData)
+                    acc.push(leftData)
 
                     return acc
                 }, [])
--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-const PORT = 3000
+const PORT = 300
 
 const fileRouter = require('./routes/file')
 
